e88afe2ed263755637e6ca0e204430796631c0b3
killyvera <david.esmeralda@gmail.com>
2023-11-08 15:27:15 -0600

    apollo server and graphql studio online

--- server.js
+++ server.js
@@ -1,7 +1,58 @@
-import { TestConection } from "./sequelize/models/index";
+// Import necessary modules
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+// Import models
 const models = require("./sequelize/models/index");
-// TestConection();
-models.sequelize.authenticate().then(() => {
-    console.log("auth correctly to oasis db")
+
+// Function to initialize the database connection
+const initializeDatabase = async () => {
+  try {
+    // Authenticate to the database
+    await models.sequelize.authenticate();
+    console.log("Successfully authenticated to the database");
+
+    // Sync the database
+    await models.sequelize.sync();
+    console.log("Database synced successfully");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+// Function to initialize the Apollo Server
+const initializeServer = async () => {
+  // Define GraphQL schema
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  // Define resolvers for the schema
+  const resolvers = {
+    Query: {
+      hello: () => "Hello Oasis",
+    },
+  };
+
+  // Create a new Apollo Server instance
+  const server = new ApolloServer({ typeDefs, resolvers, context: { models } });
+
+  // Create a new Express app
+  const app = express();
+
+  // Start the Apollo Server and apply middleware
+  await server.start();
+  server.applyMiddleware({ app });
+
+  // Start the Express server
+  app.listen({ port: 5000 }, () => {
+    console.log("Server started at http://localhost:5000" + server.graphqlPath);
+  });
+};
+
+// Initialize the database and server
+initializeDatabase().then(() => {
+  initializeServer();
 });
-models.sequelize.sync()
