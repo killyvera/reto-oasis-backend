9aa714e8b169d798aec522cf84945f838669c3eb
killyvera <david.esmeralda@gmail.com>
2023-11-08 19:22:44 -0600

    added type defs

---
+++ graphQL/typeDefs/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Desert {
+    id: ID!
+    name: String
+    location: JSON
+    createdAt: String
+    updatedAt: String
+    oases: [Oasis]
+  }
+
+  type Oasis {
+    id: ID!
+    name: String
+    location: JSON
+    waterAmount: Int
+    desertId: Int
+    createdAt: String
+    updatedAt: String
+    desert: Desert
+  }
+
+  type Query {
+    deserts: [Desert]
+    desert(id: ID!): Desert
+    oases: [Oasis]
+    oasis(id: ID!): Oasis
+  }
+
+  input DesertInput {
+    name: String
+    location: JSON
+  }
+
+  input OasisInput {
+    name: String
+    location: JSON
+    waterAmount: Int
+    desertId: Int
+  }
+
+  type Mutation {
+    createDesert(input: DesertInput): Desert
+    updateDesert(id: ID!, input: DesertInput): Desert
+    deleteDesert(id: ID!): Desert
+    createOasis(input: OasisInput): Oasis
+    updateOasis(id: ID!, input: OasisInput): Oasis
+    deleteOasis(id: ID!): Oasis
+  }
+`;
