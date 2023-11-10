1c6c0391703475192a94e992d31a71a98b691e15
killyvera <david.esmeralda@gmail.com>
2023-11-10 02:43:34 -0600

    added port to mysql connector

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -8,12 +8,13 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
     dialect: "mysql",
+    host: process.env.DB_HOST,
+    port: 3306,
   }
 );
 
-const models = require("./models")
+const models = require("./models");
 
 module.exports = {
   sequelize,
