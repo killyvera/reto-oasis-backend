a219831597d0a60addc21859d8575e13237a049f
killyvera <david.esmeralda@gmail.com>
2023-11-10 03:52:32 -0600

    change port to upgrade conection from netifly

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
-const mysql2 = require("mysql2");
+
 dotenv.config({ path: "./.env" });
 
 const sequelize = new Sequelize(
@@ -8,10 +8,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
     dialect: "mysql",
-    dialectModule: mysql2,
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: 8889,
   }
 );
 
--- sequelize/sequelizeConector.js
+++ sequelize/sequelizeConector.js
@@ -11,6 +11,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mysql",
     dialectModule: mysql2,
+    port: 8889,
   }
 );
 module.exports = sequelize;
